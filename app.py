d642d45a461f5cc188dd1ec15eb7b99d6fb13faf
ChimoMallous <emallous@usf.edu>
2025-11-21 16:03:08 -0500

    Add basic error handling for user input

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ print("Welcome to my Python program!")
 
 savings = input("How much money do you save per week? ")
 
-savings = float(savings)
+try:
+    savings = float(savings)
+except ValueError:
+    print("Please enter a valid number.")
+    exit()
+    
 yearly_savings = savings * 52
 
 print(f"You are on track to save ${yearly_savings} in a year")
